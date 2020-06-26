2a87bada9513bd79e14a66037fa552bdb666eba2
aroha_blue <amit42308@gmail.com>
2020-06-26 20:44:18 +0530

    intital commit

---
+++ src/storehash.js
@@ -0,0 +1,36 @@
+import web3 from './web3';
+//Your contract address
+const address = '0x7693ad9ed2d69ee62c67d0ec0116f055aad64b6f';
+//Your contract ABI
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "setHash",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHash",
+		"outputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+export default new web3.eth.Contract(abi, address);
